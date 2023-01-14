03e476a713cfd4df48357fdadb374f5f04032fff
mohak92 <tamhane.mohak@gmail.com>
2023-01-14 15:13:13 -0500

    Initial Commit

---
+++ UberClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UberClone
+//
+//  Created by Mohak Tamhane on 1/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
